c838a83e5a7e0503f7395395162c68b5ce785bbc
owskar <oskarganbawale@gmail.com>
2026-03-14 14:06:10 +0530

    named constant

--- answer.js
+++ answer.js
@@ -1,7 +1,7 @@
-let n = 5;
+const ROWS = 5; // number of rows in the pattern
 let num = 1;
 
-for (let i = 1; i <= n; i++) {
+for (let i = 1; i <= ROWS; i++) {
     let row = "";
 
     for (let j = 1; j <= i; j++) {
